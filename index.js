dee63ec726a32ec9e4cdeeab9d652a38741fa8b6
Jay <spacejay1007@gmail.com>
2023-01-11 02:49:47 +0900

    node

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let figlet = require("figlet");
+// let figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
-  if (err) {
-    console.log("Something wentt wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
+// figlet("Hello World!!", function (err, data) {
+//   if (err) {
+//     console.log("Something wentt wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello World");
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+// app.listen(3000);
