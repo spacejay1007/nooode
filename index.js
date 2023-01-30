ebfaa1e510dfec620a7870abe84859298799da98
Jay <spacejay1007@gmail.com>
2023-01-30 23:51:41 +0900

    node

--- index.js
+++ index.js
@@ -13,8 +13,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// terminal > node index.js > 크롬에서 localhost:3000
+
+// req (요청) , res (응답)
 app.get("/", (req, res) => {
+  // 응답에 hello World 를 보내겠다
   res.send("hello World");
+});
+
+app.get("/dog", (req, res) => {
+  // JSON ( JavaScript Object Notation ) => JSON 형태로 보내줘야함
+  res.json({ sound: "멍멍" }); // res.send({ sound: "멍멍" }) === res.json({ sound: "멍멍" });
+  // res.send("dog");
+});
+
+app.get("/cat", (req, res) => {
+  res.send({ sound: "mew" });
 });
 
 app.listen(port, () => {
